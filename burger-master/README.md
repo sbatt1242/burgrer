86b29d688da94a70308ae909b7c7aeb7fb069881
sbatt1242 <sbatt1242@gmail.com>
2017-06-13 00:21:35 -0500

    final

---
+++ burger-master/README.md
@@ -0,0 +1,5 @@
+# burger
+homework 14, node, express, handlebars
+
+
+An burger-logging app using Node, Express, and Handlebars. Add a burger to the list to be devoured. Click a button, and the burger goes into the devoured column.
---
+++ burger-master/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Homestyle', true);
